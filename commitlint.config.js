e746730529040ef2cd9f45c14c915366cc91e6d9
Pavel Kutáč <pavel.kutac@gmail.com>
2022-11-29 13:36:00 +0100

    ci: improve commit lint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,27 +1,10 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  plugins: ['commitlint-plugin-function-rules'],
   rules: {
     //   0 - Disabled, 1 - Warning, 2 - Error
     "body-max-line-length": [2, "always", 72],
-    "body-leading-blank": [1, "always"],
-    "header-max-length": [0], // level: disabled [2, "always", 72]
-    "function-rules/header-max-length": [
-      2, // level: error
-      'always',
-      (parsed) => {
-        if (parsed.scope === 'deps' || parsed.scope === 'dependencies') {
-          return [true];
-        }
-        if (parsed.header.length <= 72) {
-          return [true];
-        }
-        return [false, 'header must not be longer than 72 characters'];
-      },
-    ],
+    "header-max-length": [2, "always", 72],
     "subject-max-length": [2, "always", 50],
-    "subject-full-stop": [2, "never", "."],
-    "subject-case": [2, "always", ["lower-case"]],
     "type-enum": [
       2,
       "always",
@@ -30,7 +13,7 @@ module.exports = {
     "scope-enum": [
       2,
       "always",
-      ["logging", "sdk", "docs", "dependencies", "deps", "build", "test", "ci"],
+      ["logging", "services", "docs", "dependencies", "deps", "auth", "api", "pkg", "proto", "test", "master", "examples"],
     ],
   },
 };
